5fc62e8faf450391c8a6b786783fddcf53f836c5
Lelikbjmak <denissolovey990@gmail.com>
2023-04-24 22:45:31 +0300

    feature: Add LocalDateTimeJsonProtocol.

---
+++ src/main/scala/json/LocalDateTimeJsonProtocol.scala
@@ -0,0 +1,20 @@
+package com.innowise
+package json
+
+import spray.json.{DefaultJsonProtocol, DeserializationException, JsString, JsValue, RootJsonFormat}
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object LocalDateTimeJsonProtocol extends DefaultJsonProtocol {
+
+  implicit object LocalDateTimeJsonFormat extends RootJsonFormat[LocalDateTime] {
+    def write(instant: LocalDateTime): JsValue = JsString(DateTimeFormatter.ISO_DATE_TIME.format(instant))
+
+    def read(json: JsValue): LocalDateTime = json match {
+      case JsString(s) => LocalDateTime.parse(s, DateTimeFormatter.ISO_DATE_TIME)
+      case _ => throw DeserializationException("Expected string")
+    }
+  }
+
+}
