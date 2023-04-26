0031cf887f8021e1ef4841a2331615ba301d15b2
Lelikbjmak <denissolovey990@gmail.com>
2023-04-26 23:34:51 +0300

    feature: Add CountryServiceImplementation to fetch all possible countries.

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,7 +1,7 @@
 package com.innowise
 
 import controller.ApiRoute
-import service.impl.{ApiServiceImpl, CovidDetailsServiceImpl}
+import service.impl.{ApiServiceImpl, CountryRetrieveService, CovidDetailsServiceImpl}
 import service.{ApiService, CovidDetailsService}
 import util.{ApiConstant, ApplicationInitializer, PropertyUtil}
 
@@ -32,13 +32,13 @@ import scala.io.StdIn
 
 object Application extends App {
 
+  private val logger = LoggerFactory.getLogger(Application.getClass)
+
   private val APPLICATION_HOST = PropertyUtil.getProperty(ApiConstant.Application.HOST_PROPERTY_NAME).asInstanceOf[String]
   private val APPLICATION_PORT = PropertyUtil.getProperty(ApiConstant.Application.PORT_PROPERTY_NAME).asInstanceOf[Int]
 
   private implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "my-system")
   private implicit val executionContext: ExecutionContextExecutor = system.executionContext
-
-  private val logger = LoggerFactory.getLogger(Application.getClass)
 
   private val apiService: ApiService = new ApiServiceImpl
 
@@ -49,7 +49,9 @@ object Application extends App {
 
   private val covidDetailsService: CovidDetailsService = CovidDetailsServiceImpl(apiService, covidRepository)
 
-  private val apiRoute: ApiRoute = new ApiRoute(covidDetailsService, apiControllerRouteValidator)
+  private val countryService: CountryRetrieveService = CountryRetrieveService(countryRepository.asInstanceOf[CountryRepository])
+
+  private val apiRoute: ApiRoute = new ApiRoute(covidDetailsService, apiControllerRouteValidator, countryService)
 
   private val serverBindingFuture: Future[Http.ServerBinding] = {
     Http().newServerAt(APPLICATION_HOST, APPLICATION_PORT).bind(apiRoute.route)
@@ -57,7 +59,7 @@ object Application extends App {
 
   serverBindingFuture.onComplete { serverBinding =>
     if (serverBinding.isSuccess) {
-      logger.info(s"Server started on ${serverBinding.get.localAddress}")
+      logger.debug(s"Server started on ${serverBinding.get.localAddress}")
       ApplicationInitializer.initCountries(countryRepository, apiService)
     } else {
       logger.error(s"Server could not be started: ${serverBinding.failed.get.getMessage}")
