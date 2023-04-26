4374fbafce35ddcd9b6884da15dd6150e6d0264c
Lelikbjmak <denissolovey990@gmail.com>
2023-04-26 23:36:25 +0300

    refactor: Refactor imports.

--- src/main/scala/service/impl/CountryRetrieveService.scala
+++ src/main/scala/service/impl/CountryRetrieveService.scala
@@ -1,15 +1,14 @@
 package com.innowise
 package service.impl
 
+import dto.CountryDto
+import mapper.CountryMapper
 import repository.impl.CountryRepository
 
-import com.innowise.dto.CountryDto
-import com.innowise.mapper.CountryMapper
-
 class CountryRetrieveService(countryRepository: CountryRepository){
-  
+
   def fetchAllCountries(): List[CountryDto] = {
-    CountryMapper.mapToDtoList(countryRepository.getAllCountries())
+    CountryMapper.mapToDtoList(countryRepository.getAllCountries)
   }
-  
+
 }
